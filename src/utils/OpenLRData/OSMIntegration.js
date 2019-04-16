e4d6aca20e9834c747c5969d856d7d661fb612fd
Karel D'haene <karel.dhaene@gmail.com>
2019-04-16 14:24:27 +0200

    add integration layer

---
+++ src/utils/OpenLRData/OSMIntegration.js
@@ -0,0 +1,33 @@
+import MapDataBase from "../OpenLR/map/MapDataBase";
+import Line from "../OpenLR/map/Line";
+
+export default class OSMIntegration{
+    static initMapDataBase(lines,nodes){
+        let openLRLines = {};
+        let openLRNodes = {};
+        nodes.forEach((node)=>{
+           let openLRNode = new Node(node.id,node.lat,node.long);
+           openLRNodes[openLRNode.getID()] = openLRNodes;
+
+        });
+        lines.forEach((line)=>{
+            let openLRLine = new Line(line.id,openLRNodes[line.from],openLRNodes[line.to]);
+            if(line.frc !== undefined){
+                openLRLine.frc = OSMIntegration.getFRC(line.frc);
+            }
+            if(line.fow !== undefined){
+                openLRLine.fow = OSMIntegration.getFRC(line.fow);
+            }
+            openLRLines[openLRLine.getID()] = openLRLine;
+        });
+        return new MapDataBase(openLRLines,openLRNodes);
+    }
+    
+    static getFRC(osmvalue){
+        return 0; //todo
+    }
+
+    static getFOW(osmvalue){
+        return 0; //todo
+    }
+}
