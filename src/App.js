15ab241404f6deca153f831619922d90bc561671
Karel D'haene <karel.dhaene@gmail.com>
2019-06-03 22:25:13 +0200

    add react router to demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import {Menu} from "semantic-ui-react";
 import './App.css';
-import ApiContainer from "./components/ApiContainer";
+// import ApiContainer from "./components/ApiContainer";
 import OpenLrDemo from "./components/OpenLrDemo";
 import {MainDemo} from "./components/MainDemo";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
     render() {
         return (
-            <div>
-                {/*<ApiContainer/>*/}
-                <OpenLrDemo/>
-                {/*{<MainDemo/>}*/}
-            <div>
-        </div>
-        </div>
-    )
-        ;
+            <Router>
+                <div>
+                    <NavBar/>
+                    {/*<ul>*/}
+                        {/*<li>*/}
+                            {/*<Link to="/OpenLRDemo/">OpenLR demo page</Link>*/}
+                        {/*</li>*/}
+                        {/*<li>*/}
+                            {/*<Link to="/DiscoveryDemo/">Dataset discovery demo page</Link>*/}
+                        {/*</li>*/}
+                        {/*/!*<li>*!/*/}
+                            {/*/!*<Link to="/OpenStreetMapLinesView/">Display lines of OpenStreetMap</Link>*!/*/}
+                        {/*/!*</li>*!/*/}
+                    {/*</ul>*/}
+
+                    {/*<hr />*/}
+
+                    <Route path="/OpenLRDemo/" component={OpenLrDemo} />
+                    <Route path="/DiscoveryDemo/" component={MainDemo} />
+                    {/*<Route path="/OpenStreetMapLinesView/" component={ApiContainer} />*/}
+                </div>
+            </Router>
+        );
     }
 }
 
 export default App;
+
+
+// <ApiContainer/>
+// <OpenLrDemo/>
+// <MainDemo/>
