3835f9f5d628c023030ea6baf92aa2d5b6124978
Karel D'haene <karel.dhaene@gmail.com>
2019-06-04 15:03:37 +0200

    remove test dependency on wegenregister when not necessary

--- src/utils/Integration/OpenLR/test/LRPNodeHelper.test.js
+++ src/utils/Integration/OpenLR/test/LRPNodeHelper.test.js
@@ -1,15 +1,22 @@
-import {loadNodesLineStringsWegenregsterAntwerpen} from "../../Data/LoadData";
-import MapDataBase from "../map/MapDataBase";
-import WegenregisterAntwerpenIntegration from "../../OpenLRIntegration/WegenregisterAntwerpenIntegration";
+// import {loadNodesLineStringsWegenregsterAntwerpen} from "../../Data/LoadData";
+// import MapDataBase from "../map/MapDataBase";
+// import WegenregisterAntwerpenIntegration from "../../OpenLRIntegration/WegenregisterAntwerpenIntegration";
 import LRPNodeHelper from "../coder/LRPNodeHelper";
+import Line from "../map/Line";
+import Node from "../Map/Node";
 
-test('lrpLinesToLRPs single line in wegenregister Antwerpen',(done)=>{
+test('lrpLinesToLRPs single line from wegenregister Antwerpen',()=>{
     expect.assertions(12);
-    loadNodesLineStringsWegenregsterAntwerpen().then(features => {
-        let wegenregisterMapDataBase = new MapDataBase();
-        WegenregisterAntwerpenIntegration.initMapDataBase(wegenregisterMapDataBase,features);
+    // loadNodesLineStringsWegenregsterAntwerpen().then(features => {
+        // let wegenregisterMapDataBase = new MapDataBase();
+        // WegenregisterAntwerpenIntegration.initMapDataBase(wegenregisterMapDataBase,features);
+        //
+        // let lines = [wegenregisterMapDataBase.lines["51.16968550738436_4.399282793207395_51.16968943633098_4.399279221440794"],wegenregisterMapDataBase.lines["51.16968550738436_4.399282793207395_51.16968943633098_4.399279221440794"]];
+        let n1 = new Node("51.16968550738436_4.399282793207395",51.16968550738436,4.399282793207395);
+        let n2 = new Node("51.16968943633098_4.399279221440794",51.16968943633098,4.399279221440794);
+        let l1 = new Line("51.16968550738436_4.399282793207395_51.16968943633098_4.399279221440794",n1,n2);
+        let lines = [l1,l1];
 
-        let lines = [wegenregisterMapDataBase.lines["51.16968550738436_4.399282793207395_51.16968943633098_4.399279221440794"],wegenregisterMapDataBase.lines["51.16968550738436_4.399282793207395_51.16968943633098_4.399279221440794"]];
         let shortestPaths = [{lines:[], length:0}];
 
         let LRP = LRPNodeHelper.lrpLinesToLRPs(lines,shortestPaths);
@@ -28,6 +35,5 @@ test('lrpLinesToLRPs single line in wegenregister Antwerpen',(done)=>{
         expect(LRP[1].distanceToNext).toEqual(0);
         expect(LRP[1].seqNr).toEqual(2);
         expect(LRP[1].isLast).toEqual(true);
-        done();
-    });
-},10000);
+    // });
+});
