6b7cc67558d7dbc64f21caf832809dbcb1b1b9f9
Karel D'haene <karel.dhaene@gmail.com>
2019-06-04 14:46:34 +0200

    change var to let

--- src/someTests.test.js
+++ src/someTests.test.js
@@ -30,6 +30,6 @@ test.skip('speed ophalen catalogus na doorsturen',(done)=>{
 
 function clock(start) {
     if ( !start ) return process.hrtime();
-    var end = process.hrtime(start);
+    let end = process.hrtime(start);
     return Math.round((end[0]*1000) + (end[1]/1000000));
 }
