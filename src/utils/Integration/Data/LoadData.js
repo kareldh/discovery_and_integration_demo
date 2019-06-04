1d3b86ee6dae8c2305038e826152b719fd941e0b
Karel D'haene <karel.dhaene@gmail.com>
2019-06-04 17:47:49 +0200

    use https to download wegenregister

--- src/utils/Integration/Data/LoadData.js
+++ src/utils/Integration/Data/LoadData.js
@@ -5,7 +5,7 @@ import {DATASET_URL} from "./const";
 
 export function loadNodesLineStringsWegenregsterAntwerpen(){
     return new Promise((resolve, reject) => {
-        axios.get("http://portaal-stadantwerpen.opendata.arcgis.com/datasets/6bad868c084a43ef8031cfe1b96956b2_297.geojson ")
+        axios.get("https://portaal-stadantwerpen.opendata.arcgis.com/datasets/6bad868c084a43ef8031cfe1b96956b2_297.geojson ")
             .then((data) => { resolve(data.data.features) })
             .catch((error) => { reject(error) })
     });
