0d02cea4593796b095fbf77f6d548e9ccbe55f9d
Karel D'haene <karel.dhaene@gmail.com>
2019-06-04 17:45:35 +0200

    fix github pages routing issues when refreshing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route} from "react-router-dom";
 import './App.css';
 // import ApiContainer from "./components/ApiContainer";
 import OpenLrDemo from "./components/OpenLrDemo";
@@ -9,25 +9,11 @@ import NavBar from "./components/NavBar";
 class App extends Component {
     render() {
         return (
-            <Router basename={process.env.PUBLIC_URL+"/discovery_and_integration_demo/"}>
+            <Router>
                 <div>
                     <NavBar/>
-                    {/*<ul>*/}
-                        {/*<li>*/}
-                            {/*<Link to="/OpenLRDemo/">OpenLR demo page</Link>*/}
-                        {/*</li>*/}
-                        {/*<li>*/}
-                            {/*<Link to="/DiscoveryDemo/">Dataset discovery demo page</Link>*/}
-                        {/*</li>*/}
-                        {/*/!*<li>*!/*/}
-                            {/*/!*<Link to="/OpenStreetMapLinesView/">Display lines of OpenStreetMap</Link>*!/*/}
-                        {/*/!*</li>*!/*/}
-                    {/*</ul>*/}
-
-                    {/*<hr />*/}
-
-                    <Route path="/OpenLRDemo/" component={OpenLrDemo}  />
-                    <Route path="/DiscoveryDemo/" component={MainDemo} />
+                    <Route path={"/OpenLRDemo/"} component={OpenLrDemo}  />
+                    <Route path={"/DiscoveryDemo/"} component={MainDemo} />
                     {/*<Route path="/OpenStreetMapLinesView/" component={ApiContainer} />*/}
                 </div>
             </Router>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,7 +13,7 @@ class NavBar extends React.Component {
         return (
             <Menu>
                 <Menu.Item
-                    as={Link} to='/OpenLRDemo/'
+                    as={Link} to={'/OpenLRDemo/'}
                     name='OpenLRDemo'
                     active={activeItem === 'OpenLRDemo'}
                     onClick={this.handleItemClick}
@@ -21,7 +21,7 @@ class NavBar extends React.Component {
                     OpenLR demo page
                 </Menu.Item>
                 <Menu.Item
-                    as={Link} to='/DiscoveryDemo/'
+                    as={Link} to={'/DiscoveryDemo/'}
                     name='DiscoveryDemo'
                     active={activeItem === 'DiscoveryDemo'}
                     onClick={this.handleItemClick}
